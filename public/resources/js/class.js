bb150b424a3e3cbdeeade433713cf18d975c7dfe
William Wang <william.oliver.wang@gmail.com>
2018-07-18 18:04:56 -0400

    restructured project, added boiler plate code for server.js

---
+++ public/resources/js/class.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+    $('.class-open').click(function(event){  
+        $(event.currentTarget).parent().hide();
+        $(event.currentTarget).fadeOut(300, function(){
+            $(event.currentTarget).parent().next().fadeIn(300);
+        });
+    });
+    $('.register-button').click(function(event){    
+        $(event.currentTarget).fadeOut(300, function(){
+            $(event.currentTarget).prev().show();
+            $(event.currentTarget).prev().children().fadeIn(300);
+        });
+    });
+    
+    // Only execute this jquery if browser width is desktop size on page load
+    if($(window).width() >= 1538.02){
+        $('.column-left').css("height", "auto");
+        $('.column-right').height($('.column-left').height());
+        $(".class-comments").css("max-height", ($(".column-right").height()-$(".class-rating").height()-$(".add-comments").height()));
+    }
+    
+    // fix for if they resize window manually
+    $(window).resize(function() {
+        if( $(window).width() > 1538.02) {
+            $('.column-left').css("height", "auto");
+            $('.column-right').height($('.column-left').height());
+            $(".class-comments").css("max-height", ($(".column-right").height()-$(".class-rating").height()-$(".add-comments").height()));
+        }
+    });
+});
+
+// Turn on Bootstrap tooltips
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const Knex = require('knex');
+var session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+
+
+// connect to postgres db
+const knex = Knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'course-planner'
+    }
+});
+
+const store = new KnexSessionStore({
+    knex: knex,
+});
+
+
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    name: 'user_id'
+    secret: 'changethisbeforedeploy',
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    unset: 'destroy'
+}));
+
+
+// route for handling 404 requests(unavailable routes)
+app.use(function (req, res, next) {
+  res.status(404).send("404 Page Not Found")
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+  console.log('Server running on port' + PORT);
+})
