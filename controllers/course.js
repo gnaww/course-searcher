fb3264dc33782654427857d4e6a39cc6a6296bdf
fireteam99 <raymancheungsy@gmail.com>
2018-07-28 22:46:21 -0400

    basic course info

--- controllers/course.js
+++ controllers/course.js
@@ -16,34 +16,37 @@ const handleCourseGet = async (req, res, next) =>  {
     try {
         const selected_course = await Course.query().where('course_full_number', course_id);
         console.log(selected_course);
-        if (selected_course === undefined || selected_course.length == 0) {
+        if (selected_course === undefined || selected_course.length === 0) {
             return res.status(404).json('Course not found');
         } else {
             const first_section = selected_course[0];
-            const { name, course_full_number, core_codes, credits, notes } = first_section;
+            const { name, course_full_number, core_codes, credits, pre_reqs, notes } = first_section;
             let core_codes_string = '';
-
+            
             // formats the core codes
-            if (core_codes) {
+            if (Object.keys(core_codes).length === 0 && core_codes.constructor === Object) {
+                core_codes_string = 'None';
+            } else {
                 core_codes.forEach(function(element) {
                     core_codes_string += element.code + ', ';
                 });
                 core_codes_string = core_codes_string.substring(0, core_codes_string.length - 2);
-            } else {
-                core_codes_string = 'None';
             }
 
             data.name = name;
             data.course_full_number = course_full_number;
             data.core_codes_string = core_codes_string;
             data.credits = credits;
+            data.pre_reqs = pre_reqs;
             data.notes = notes;
-
+            
+            console.log('DEBUGGING INFO ----------------------------------')
             console.log(name);
             console.log(course_full_number);
             console.log(core_codes_string);
             console.log(credits);
             console.log(notes);
+            console.log('-------------------------------------------------')
 
             res.render('pages/course', data);
         }
