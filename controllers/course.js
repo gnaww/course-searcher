390c6fec263c01db30a6b861db6c3d4150a78b44
fireteam99 <raymancheungsy@gmail.com>
2018-07-29 00:41:27 -0400

    issues with time field

--- controllers/course.js
+++ controllers/course.js
@@ -14,13 +14,14 @@ const handleCourseGet = async (req, res, next) =>  {
     }
     const course_id = req.query.id;
     try {
+        // course information
         const selected_course = await Course.query().where('course_full_number', course_id);
         console.log(selected_course);
         if (selected_course === undefined || selected_course.length === 0) {
             return res.status(404).json('Course not found');
         } else {
             const first_section = selected_course[0];
-            const { name, course_full_number, core_codes, credits, pre_reqs, notes } = first_section;
+            const { name, course_full_number, core_codes, credits, pre_reqs, notes, time} = first_section;
             let core_codes_string = '';
             
             // formats the core codes
@@ -32,7 +33,8 @@ const handleCourseGet = async (req, res, next) =>  {
                 });
                 core_codes_string = core_codes_string.substring(0, core_codes_string.length - 2);
             }
-
+            
+            
             data.name = name;
             data.course_full_number = course_full_number;
             data.core_codes_string = core_codes_string;
@@ -40,12 +42,24 @@ const handleCourseGet = async (req, res, next) =>  {
             data.pre_reqs = pre_reqs;
             data.notes = notes;
             
+            // section information
+            /* section object will contain the following properties:
+                *   section_number
+                *   index_number
+                *   time
+                *   location
+            */
+            
+            data.sections = [];
+            
             console.log('DEBUGGING INFO ----------------------------------')
             console.log(name);
             console.log(course_full_number);
             console.log(core_codes_string);
             console.log(credits);
+            console.log(pre_reqs);
             console.log(notes);
+            console.log(time);
             console.log('-------------------------------------------------')
 
             res.render('pages/course', data);
