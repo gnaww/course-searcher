70d1c80a6dae7c823db42b8b73191324a2ca95d8
William Wang <william.oliver.wang@gmail.com>
2018-08-01 01:56:45 -0400

    finished backend for getting user courses and displaying it on account page

--- controllers/account.js
+++ controllers/account.js
@@ -1,5 +1,3 @@
-const User = require('../models/User');
-
 const displayAccount = (knex) => async (req, res) => {
     const username = req.session.user
     let data = {
@@ -10,30 +8,33 @@ const displayAccount = (knex) => async (req, res) => {
         data.notification = req.session.notification;
         req.session.notification = null;
     }
-    const userCourses = await User
-        .query()
-        .eager('courses')
-        .modifyEager('courses', builder => {
-            builder.groupBy('course_full_number');
-        })
-        .where({
-            username: username
-        })
-        .then(result => {
-            console.log(result[0])
-            let coursesArray = result[0].courses.map(course => {
-                return `(${course.course_full_number}) ${course.name}`;
+    knex.raw(`SELECT DISTINCT ON (course_full_number)
+                course_full_number, name, users_courses.semester
+                FROM courses INNER JOIN users_courses
+                ON courses.course_full_number = users_courses.course
+                WHERE users_courses.username = '${username}'`)
+        .then(results => {
+            let semesters = {};
+            results.rows.forEach(course => {
+                let sem = course.semester;
+                if (semesters[sem]) {
+                    semesters[sem].push(`(${course.course_full_number}) ${course.name}`);
+                } else {
+                    semesters[sem] = [];
+                    semesters[sem].push(`(${course.course_full_number}) ${course.name}`);
+                }
             });
-            data.courses = coursesArray;
+            data.semesters = semesters;
+            res.render('pages/account', data);
         })
         .catch(err => {
-            console.log(err.stack);
+            console.log('error occurred while querying users_courses: ', err.stack);
             data.notification = {
                 type: 'error',
                 message: 'Error retrieving account information. Something went wrong on our end :('
             };
+            res.render('pages/account', data);
         });
-    res.render('pages/account', data);
 }
 
 module.exports = {
--- seeds/users_courses.js
+++ seeds/users_courses.js
@@ -6,9 +6,9 @@ exports.seed = function(knex, Promise) {
                 { username: 'will', course: '33:010:272', semester: 'Transfer Courses' },
                 { username: 'will', course: '33:010:273', semester: 'External Examinations' },
                 { username: 'will', course: '33:010:275', semester: 'Fall 2018' },
-                { username: 'ray', course: '33:010:272', semester: 'Transfer Courses' },
-                { username: 'ray', course: '33:010:273', semester: 'External Examinations' },
-                { username: 'ray', course: '33:010:275', semester: 'Fall 2018' }
+                { username: 'ray', course: '33:010:310', semester: 'Transfer Courses' },
+                { username: 'ray', course: '33:010:325', semester: 'External Examinations' },
+                { username: 'ray', course: '33:010:326', semester: 'Fall 2018' }
             ]);
         });
 };
