a54c3708d37b3712728063171102848e5fe8d958
fireteam99 <raymancheungsy@gmail.com>
2018-08-01 14:16:57 -0400

    cleaned up the code and fixed many of the bugs that had to do with sections missing properties

--- controllers/course.js
+++ controllers/course.js
@@ -1,148 +1,137 @@
 const Course = require('../models/Course.js');
 
-const handleCourseGet = async (req, res, next) =>  {
-    let data = {
-        notification: null,
-        user: null
-    };
-    if (req.session.notification) {
-        data.notification = req.session.notification;
-        req.session.notification = null;
-    }
-    if (req.session.user) {
-        data.user = req.session.user;
-    }
-    const course_id = req.query.id;
-    try {
-        // course information
-        const selected_course = await Course.query().where('course_full_number', course_id);
-        if (selected_course === undefined || selected_course.length === 0) {
-            return res.status(404).json('Course not found');
-        } else {
-            const first_section = selected_course[0];
-            const { name, course_full_number, core_codes, credits, pre_reqs, notes, times} = first_section;
-            let core_codes_string = '';
+const handleCourseGet = async (req, res, next) => {
+  let data = {
+    notification: null,
+    user: null
+  };
+  if (req.session.notification) {
+    data.notification = req.session.notification;
+    req.session.notification = null;
+  }
+  if (req.session.user) {
+    data.user = req.session.user;
+  }
+  const course_id = req.query.id;
+  try {
+    // course information
+    const selected_course = await Course.query().where('course_full_number', course_id);
+    if (selected_course === undefined || selected_course.length === 0) {
+      return res.status(404).json('Course not found');
+    } else {
+      const first_section = selected_course[0];
+      // destructuring
+      const {
+        name,
+        course_full_number,
+        core_codes,
+        credits,
+        pre_reqs,
+        times
+      } = first_section;
 
-            // formats the core codes
-            if (Object.keys(core_codes).length === 0 && core_codes.constructor === Object) {
-                core_codes_string = 'None';
-            } else {
-                core_codes.forEach(function(element) {
-                    core_codes_string += element.code + ', ';
-                });
-                core_codes_string = core_codes_string.substring(0, core_codes_string.length - 2);
-            }
-
-
-            data.name = name;
-            data.course_full_number = course_full_number;
-            data.core_codes_string = core_codes_string;
-            data.credits = credits;
-            data.pre_reqs = pre_reqs;
-            data.notes = notes;
-
-            // section information
-            /* section object will contain the following properties:
-                *   section_number
-                *   index_number
-                *   day_time: day and am/pm time
-                *   location: campus and room
-                *   CURRENTLY UNUSED:
-                *   meeting_mode
-            */
-            /*
-                *   meeting mode code:
-                *   90 -> ONLINE INSTRUCTION(INTERNET)
-                *   02 -> LEC
-                *   03 -> RECIT
-                *   04 -> SEM
-                *   19 -> PROJ-IND
-            */
-            console.log('SECTION DEBUTTING INFO ----------------------------------');
-            console.log('Amount of sections: ' + selected_course.length);
-            let sections = [];
-            for (let i = 0; i < selected_course.length; i++) {
-              let section_number = selected_course[i].section_number;
-              let index_number = selected_course[i].section_index;
-
-              let section_times = selected_course[i].times;
-
-              let day_times = [];
-              let locations = [];
-              let meeting_codes = [];
-              let meeting_mode_descs = [];
-
-              console.log('Amount of classes for section' + i +': ' + section_times.length);
-              for (let j = 0; j < section_times.length; j++) {
-                let pm_code = section_times[i].pmCode; // not returned
-                let day_time = 'Error';
-
-                // format location
-                let location = 'Error'
-
-                // meeting mode
-                let meeting_code = section_times[i].meetingModeCode;
-                let meeting_mode_desc = section_times[i].meetingModeDesc;
-
-                // take care of time and location
-                if (meeting_code === '90') { // online course
-                  let time = 'Online Course';
-                  location = 'Online Course';
-                } else if(meeting_code === '19') {
-                  let time = 'Project/Independent';
-                  location = 'Project/Independent';
-                } else {
-                  // time
-                  let start_time = section_times[i].startTime;
-                  let end_time = section_times[i].endTime;
-                  let day = formatDay(section_times[i].meetingDay);
-                  let time = formatTime(start_time, end_time, pm_code);
-                  day_time = day + " " + time;
-                  // loaction
-                  let campus = section_times[i].campusAbbrev;
-                  let building = section_times[i].buildingCode;
-                  let room = section_times[i].roomNumber;
-                  location = campus + ' ' + building + ' ' + room;
-
-                  day_times.push(day_time);
-                  locations.push(location);
-                  meeting_codes.push(meeting_code);
-                  meeting_mode_descs.push(meeting_mode_desc);
-                }
-              }
+      // formats the core codes
+      let core_codes_string = '';
+      if (Object.keys(core_codes).length === 0 && core_codes.constructor === Object) {
+        core_codes_string = 'None';
+      } else {
+        core_codes.forEach(function(element) {
+          core_codes_string += element.code + ', ';
+        });
+        core_codes_string = core_codes_string.substring(0, core_codes_string.length - 2);
+      }
 
 
-              let section = {
-                section_number: section_number,
-                index_number: index_number,
-                day_times: day_times, // is array
-                locations: locations, // is array
-                meeting_codes: meeting_codes, // is array
-                meeting_mode_descs: meeting_mode_descs // is array
-              }
-              console.log('section' + i);
-              console.log(section);
-              sections.push(section);
-            }
-            console.log('-------------------------------------------------')
+      data.name = name;
+      data.course_full_number = course_full_number;
+      data.core_codes_string = core_codes_string;
+      data.credits = credits;
+      data.pre_reqs = pre_reqs;
+      data.description = 'Coming Soon';
 
-            console.log('CLASS DEBUGGING INFO ----------------------------------')
-            console.log('name: ' + name);
-            console.log('course_full_number: ' + course_full_number);
-            console.log('core_codes_string: ' + core_codes_string);
-            console.log('credits: ' + credits);
-            console.log('pre_reqs: ' + pre_reqs);
-            console.log('notes: ' + notes);
-            console.log('times: ' + times);
-            console.log('sections: ' + sections);
-            console.log('-------------------------------------------------')
+      // section information
+      /* section object will contain the following properties:
+       *   section_number
+       *   section_index
+       *   day_time: day and am/pm time
+       *   location: campus and room
+       *   CURRENTLY UNUSED:
+       *   meeting_mode
+       */
+      /*
+       *   meeting mode code:
+       *   90 -> ONLINE INSTRUCTION(INTERNET)
+       *   02 -> LEC
+       *   03 -> RECIT
+       *   04 -> SEM
+       *   19 -> PROJ-IND
+       */
+      console.log('SECTION DEBUGGING INFO ----------------------------------');
+      console.log('Amount of sections: ' + selected_course.length);
+      let sections = [];
+      for (let i = 0; i < selected_course.length; i++) {
+        let section_number = selected_course[i].section_number;
+        let section_index = selected_course[i].section_index;
+        let notes = selected_course[i].notes;
 
-            res.render('pages/course', data);
+        let section_times = selected_course[i].times;
+
+        let day_times = [];
+        let locations = [];
+        let meeting_codes = [];
+        let meeting_mode_descs = [];
+
+        console.log('Amount of classes for section' + i + ': ' + section_times.length);
+        for (let j = 0; j < section_times.length; j++) {
+
+          // meeting mode
+          let meeting_code = section_times[j].meetingModeCode;
+          let meeting_mode_desc = section_times[j].meetingModeDesc;
+
+          // time and location
+          let day_time = formatTimeDay(section_times[j], meeting_code);
+          let location = formatLocation(section_times[j], meeting_code);
+
+          day_times.push(day_time);
+          locations.push(location);
+          meeting_codes.push(meeting_code);
+          meeting_mode_descs.push(meeting_mode_desc);
         }
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json('Error retrieving course data');
+
+
+        let section = {
+          section_number: section_number,
+          section_index: section_index,
+          notes: notes,
+          day_times: day_times, // is array
+          locations: locations, // is array
+          meeting_codes: meeting_codes, // is array
+          meeting_mode_descs: meeting_mode_descs // is array
+        }
+        console.log('section' + i);
+        console.log(section);
+        sections.push(section);
+      }
+      console.log('-------------------------------------------------')
+
+      console.log('CLASS DEBUGGING INFO ----------------------------------')
+      console.log('name: ' + name);
+      console.log('course_full_number: ' + course_full_number);
+      console.log('core_codes_string: ' + core_codes_string);
+      console.log('credits: ' + credits);
+      console.log('pre_reqs: ' + pre_reqs);
+      console.log('notes: ' + notes);
+      console.log('times: ' + times);
+      console.log('sections:');
+      console.log(sections);
+      console.log('-------------------------------------------------')
+
+      res.render('pages/course', data);
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json('Error retrieving course data');
+  }
 }
 
 const formatDay = (day) => {
@@ -166,22 +155,35 @@ const formatDay = (day) => {
       return 'Sat';
       break;
     default:
-      return 'Day Error';
+      return 'N/A';
   }
 }
 
 /*
-    *   since there are no classes at midnight we can assume that
-    *   any time where there is a pm code of P and the start timeout
-    *   is greater than the end time, that the class goes from AM to PM
-    *   or if the end time takes place from 12:00 - 12:59 because classes
-    *   are always longer than 59 minutes
-*/
-const formatTime = (start_time, end_time, pm_code) => {
-  if (parseInt(start_time) > parseInt(end_time) || (parseInt(end_time) >= 1200 && parseInt(end_time) <= 1259)) {
-    return (stripLeadingZero(start_time) + "AM - " + stripLeadingZero(end_time) + "PM");
+ *   since there are no classes at midnight we can assume that
+ *   any time where there is a pm code of P and the start timeout
+ *   is greater than the end time, that the class goes from AM to PM
+ *   or if the end time takes place from 12:00 - 12:59 because classes
+ *   are always longer than 59 minutes
+ */
+const formatTime = (times, meeting_code) => {
+  // check for meeting type
+  if (meeting_code === '90') {
+    return 'Online';
+  } else if (meeting_code === '19') {
+    return 'Independent Project';
   } else {
-    return (stripLeadingZero(start_time) + pm_code + "M - " + stripLeadingZero(end_time) + pm_code + "M");
+    // fall back incase time passed is null
+    let time;
+    if (times.startTime == null || times.endTime == null) {
+      return 'N/A';
+    } else {
+      if (parseInt(start_time) > parseInt(end_time) || (parseInt(end_time) >= 1200 && parseInt(end_time) <= 1259)) {
+        return (stripLeadingZero(start_time) + "AM - " + stripLeadingZero(end_time) + "PM");
+      } else {
+        return (stripLeadingZero(start_time) + pm_code + "M - " + stripLeadingZero(end_time) + pm_code + "M");
+      }
+    }
   }
 }
 
@@ -199,14 +201,38 @@ const stripLeadingZero = (str) => {
 }
 
 const addColon = (str) => {
-    return str.slice(0,2) + ":" + str.slice(2,4);
+  return str.slice(0, 2) + ":" + str.slice(2, 4);
 }
-// formatTime('11:00', '12:00', 'P');
+// formatTimeDay('11:00', '12:00', 'P');
 
-const formatLocation = (campus_abbrev, building_code, room_number) => {
-  return campus_abbrev + " " + building_code + " " + room_number;
+const formatTimeDay = (times, meeting_code) => {
+  if (meeting_code === '90' || meeting_code === '19') {
+    return formatTime(times, meeting_code);
+  }
+  return formatDay(times, meeting_code) + " " +  formatTime(times, meeting_code);
+}
+
+const formatLocation = (times, meeting_code) => {
+  if (meeting_code === '90') {
+    return 'Online';
+  } else if (meeting_code === '19') {
+    return 'Independent Project';
+  } else {
+    // make sure there is a fall back in case any location properties are null
+    let = { campus_abbrev, building_code, room_number } = times;
+    if (campus_abbrev == null) {
+      campus_abbrev = 'N/A';
+    }
+    if (building_code == null) {
+      building_code = 'N/A';
+    }
+    if (room_number == null) {
+      room_number = 'N/A';
+    }
+    return campus_abbrev + " " + building_code + " " + room_number;
+  }
 }
 
 module.exports = {
-    handleCourseGet: handleCourseGet
+  handleCourseGet: handleCourseGet
 }
