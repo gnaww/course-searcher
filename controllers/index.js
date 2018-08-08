e73251c8732ab4ea02b5a634cf480b8667c45b91
William Wang <william.oliver.wang@gmail.com>
2018-08-08 02:34:55 -0400

    got sql query working

--- controllers/index.js
+++ controllers/index.js
@@ -54,21 +54,36 @@ const requirementSearch = (params, req, res, knex) => {
         //     .then(results => {
         //         // console.log(results[0].courses[0].core_codes);
         //     });
-        knex.raw(`SELECT * FROM (
-                      SELECT DISTINCT ON (course_full_number)
-                      course_full_number, name, core_codes, pre_reqs, section_open_status
-                      FROM courses AS c INNER JOIN courses_requirements AS cr
-                      ON c.course_full_number = cr.course
-                      GROUP BY cr.course
-                      ORDER BY course_full_number
-                  ) t
-                  ORDER BY COUNT(cr.course)`)
+        // knex.raw(`SELECT * FROM (
+        //               SELECT DISTINCT ON (course_full_number)
+        //               course_full_number, name, core_codes, pre_reqs, section_open_status
+        //               FROM courses AS c INNER JOIN courses_requirements AS cr
+        //               ON c.course_full_number = cr.course
+        //           ) t
+        //           GROUP BY (t.course_full_number)
+        //           ORDER BY COUNT(t.course_full_number)`)
                    //WHERE cr.requirement = 'AHo' OR cr.requirement = 'AHp'
+        knex.raw(`SELECT * FROM
+                  (
+                      SELECT DISTINCT ON (course_full_number)
+                      course_full_number, name, core_codes, pre_reqs, section_open_status, t.count as count
+                      FROM COURSES AS c INNER JOIN
+                      (
+                          SELECT course, COUNT(course) AS count FROM courses_requirements
+                          WHERE requirement = 'AHp' OR requirement = 'AHo' OR requirement = 'AHq'
+                          GROUP BY course
+                      ) t
+                      ON c.course_full_number = t.course
+                  ) a
+                  ORDER BY a.count DESC`)
             .then(result => {
+                // console.log(result.rows);
                 result.rows.forEach(course => {
+                    // console.log(course.count);
                     course.core_codes.forEach(code => {
                         process.stdout.write(code.code + ' ');
                     });
+                    process.stdout.write(course.count + ' ' + course.name + ' ' + course.course_full_number);
                     console.log();
                 });
             });
