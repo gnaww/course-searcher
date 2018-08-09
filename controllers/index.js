d519894c5a781b9d675bf79677be2fe2a27ff438
William Wang <william.oliver.wang@gmail.com>
2018-08-09 01:01:13 -0400

    implemented search query validation on backend and implemented preopulating search form based on GET

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,9 @@ const dump = require('dumper.js/src/dump');
 const displayHomepage = knex => (req, res) => {
     let data = {
         notification: null,
-        user: null
+        user: null,
+        form: null,
+        results: null
     };
     if (req.session.notification) {
         data.notification = req.session.notification;
@@ -15,17 +17,24 @@ const displayHomepage = knex => (req, res) => {
     }
 
     if (req.query.search === 'requirement') {
-        let results = requirementSearch(req.query, req, res, knex);
-        if (results === 'error') {
+        let query = requirementSearch(req.query, req, res, knex);
+        if (query === 'error') {
             return;
         } else {
-            data.results = results;
+            data.results = query.results;
+            data.form = query.form;
         }
     }
     else if (req.query.search === 'direct') {
-        data.results = directSearch(req.query, req, res, knex);
+        let query = directSearch(req.query, req, res, knex);
+        if (query === 'error') {
+            return;
+        } else {
+            data.results = query.results;
+            data.form = query.form;
+        }
     }
-
+    console.log(req.url)
     res.render('pages/index', data);
 }
 
@@ -33,28 +42,32 @@ const requirementSearch = (params, req, res, knex) => {
     dump(params);
     let numRequirements = 0;
     let requirements = [];
+    let formRequirements = {
+        NS: false,
+        SCL: false,
+        HST: false,
+        WC: false,
+        WCr: false,
+        WCd: false,
+        CC: false,
+        QQ: false,
+        QR: false,
+        AHo: false,
+        AHp: false,
+        AHq: false,
+        AHr: false,
+        ITR: false
+    };
+
     Object.keys(params).forEach(key => {
         if (isRequirement(key)) {
+            formRequirements[key] = true;
             requirements.push(key);
             numRequirements++;
         }
     });
     dump(requirements);
-    // const requirement = await Requirement
-    //     .query()
-    //     .eager('courses')
-    //     .then(results => {
-    //         // console.log(results[0].courses[0].core_codes);
-    //     });
-    // knex.raw(`SELECT * FROM (
-    //               SELECT DISTINCT ON (course_full_number)
-    //               course_full_number, name, core_codes, pre_reqs, section_open_status
-    //               FROM courses AS c INNER JOIN courses_requirements AS cr
-    //               ON c.course_full_number = cr.course
-    //           ) t
-    //           GROUP BY (t.course_full_number)
-    //           ORDER BY COUNT(t.course_full_number)`)
-               //WHERE cr.requirement = 'AHo' OR cr.requirement = 'AHp'
+    dump(formRequirements);
     if (numRequirements > 4) {
         req.session.notification = {
             type: 'error',
@@ -65,11 +78,11 @@ const requirementSearch = (params, req, res, knex) => {
     } else if (!Array.isArray(requirements) || !requirements.length) {
         req.session.notification = {
             type: 'error',
-            message: 'Error searching by requirement! Must select at least one requirement checkbox'
+            message: 'Error searching by requirement! Must select at least one requirement checkbox.'
         }
         res.redirect('/');
         return 'error';
-    } else {
+    } else { // valid search query
         knex.raw(`SELECT * FROM
                  (
                      SELECT DISTINCT ON (course_full_number)
@@ -88,17 +101,52 @@ const requirementSearch = (params, req, res, knex) => {
                 result.rows.forEach(course => {
                     // console.log(course.count);
                     course.core_codes.forEach(code => {
-                        process.stdout.write(code.code + ' ');
+                        // process.stdout.write(code.code + ' ');
                     });
-                    process.stdout.write(course.count + ' ' + course.name + ' ' + course.course_full_number + ' ' + course.section_open_status);
-                    console.log();
+                    // process.stdout.write(course.count + ' ' + course.name + ' ' + course.course_full_number + ' ' + course.section_open_status);
+                    // console.log();
                 });
             });
+        return { results: [], form: { search: 'requirement', requirements: formRequirements }}; //replace [] with db query result
     }
 }
 
 const directSearch = (params, req, res, knex) => {
-    console.log('direct search');
+    dump(params);
+    let formCategory = {
+        default: true,
+        keyword: false,
+        index: false,
+        course: false,
+        professor: false
+    };
+    if (params.category && params.query) {
+        let category = params.category;
+        if (category === 'keyword' || category === 'index' || category === 'course' || category === 'professor') { // valid search query
+            formCategory[category] = true;
+            formCategory.default = false;
+            let formDirect = {
+                search: 'direct',
+                category: formCategory,
+                query: params.query
+            };
+            return {  results: [], form: formDirect }; //replace the [] with the database query result
+        } else {
+            req.session.notification = {
+                type: 'error',
+                message: 'Error searching courses directly! An invalid search category was chosen.'
+            }
+            res.redirect('/');
+            return 'error';
+        }
+    } else {
+        req.session.notification = {
+            type: 'error',
+            message: 'Error searching courses directly! Both a category and query string must be entered.'
+        }
+        res.redirect('/');
+        return 'error';
+    }
 }
 
 const isRequirement = key => {
--- public/js/index.js
+++ public/js/index.js
@@ -12,11 +12,53 @@ $( document ).ready(function() {
         $('#requirement-select').removeClass('btn-active');
         $('#direct-select').addClass('btn-active');
     });
+
+    $('#requirement-asc').click(function() {
+        redirect('requirement-asc')
+    });
+
+    $('#requirement-desc').click(function() {
+        redirect('requirement-desc');
+    });
+
+    $('#number-asc').click(function() {
+        redirect('number-asc');
+    });
+
+    $('#number-desc').click(function() {
+        redirect('number-desc');
+    });
+
+    $('#name-asc').click(function() {
+        redirect('name-asc');
+    });
+
+    $('#name-desc').click(function() {
+        redirect('name-desc');
+    });
+
+    $('#status-asc').click(function() {
+        redirect('status-asc');
+    });
+
+    $('#status-desc').click(function() {
+        redirect('status-desc');
+    });
 });
 
-// get current url: document.URL
-// check if personalize is checked  var checkedValue = document.querySelector('#personalize:checked').value;\
-// redirect to url window.location.href = "http://stackoverflow.com";
+const redirect = sortType => {
+    const personalize = document.getElementById('personalize').checked;
+    let url = document.URL;
+    let params = sortType.split('-');
+
+    const sortIndex = url.indexOf('&sort') === -1 ? Number.MAX_VALUE : url.indexOf('&sort');
+    const orderIndex = url.indexOf('&order') === -1 ? Number.MAX_VALUE : url.indexOf('&order');
+    const personalizeIndex = url.indexOf('&personalize') === -1 ? Number.MAX_VALUE : url.indexOf('&personalize');
+
+    url = url.slice(0, Math.min(sortIndex, orderIndex, personalizeIndex));
+    url = `${url}&sort=${params[0]}&order=${params[1]}&personalize=${personalize}#search-results`;
+    window.location.href = url;
+};
 
 // Enable Bootstrap tooltips
 $(function () {
