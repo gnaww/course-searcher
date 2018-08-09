77df9a697cf2c4177a48da00f2882de9c1e36b5a
William Wang <william.oliver.wang@gmail.com>
2018-08-09 19:32:45 -0400

    change knexconfig

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: 'redskies',
+        password: '',
         database: 'course-planner'
     }
 };
