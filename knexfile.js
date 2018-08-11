362152d061b00f0ed8a7651dc302dfce6688f812
fireteam99 <raymancheungsy@gmail.com>
2018-08-11 00:24:13 -0400

    update function now runs at start of server and  requirements bug is now fixed

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: '',
+        password: 'redskies',
         database: 'course-planner'
     }
 };
