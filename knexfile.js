caa0f143c87ae5a33dbb880259dfdc25dfad8bca
William Wang <william.oliver.wang@gmail.com>
2018-08-20 01:21:49 -0400

    remove pw

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
         connection: {
             host: '127.0.0.1',
             user: 'postgres',
-            password: 'redskies',
+            password: '',
             database: 'course-planner'
         }
     },
